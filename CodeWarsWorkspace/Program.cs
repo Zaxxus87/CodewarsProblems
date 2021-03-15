9c01e223dd6ce7c1d33b470514292f674e2a344c
Jeffrey Baker <jeffrey1871@gmail.com>
2021-03-14 22:16:00 -0500

    Initial Commit

---
+++ CodeWarsWorkspace/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CodeWarsWorkspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(calculate("1+2"));
+        }
+
+        public static double calculate(String s)
+        {
+            double ans = 0.0;
+            foreach (Char c in s)
+                if (Char.IsDigit(c))
+                    ans += c;
+            return ans;
+        }
+    }
+}
