aad59e5b6c67e7ddfcccaacf3d049606919ad20f
Jeffrey Baker <jeffrey1871@gmail.com>
2021-05-08 13:31:50 -0500

    Very Mathy Problem

--- CodeWarsWorkspace/Program.cs
+++ CodeWarsWorkspace/Program.cs
@@ -10,32 +10,35 @@ namespace CodeWarsWorkspace
     {
         static void Main(string[] args)
         {
-            string guess = Buddy(48, 50);
-            foreach (var item in collection)
-            {
-
-            }
-
-            //{ 20, 20, 19, 16, 10, 0 });
+            var theAns = Solve(12, 20, 18, 45, 30, 60);
+            foreach(int num in theAns)
+                Console.WriteLine(num);
+            //[15, 135, 49, 18]
 
         }
 
 
-        public static string Buddy(long start, long limit)
+        public static int[] Solve(int a, int b, int c, int alpha, int beta, int gamma)
         {
-            List<int> buddy1 = new List<int>() { 1 };
-            List<int> buddy2 = new List<int>() { 1 };
-            for (int i = (int)start; i <= limit; i++)
-            {
-                int stop = (int)(Math.Sqrt(i));
-                for (int j = 2; j < stop; j++)
-                    if (i % j == 0)
-                    {
-                        buddy1.Add(j);
-                        buddy2.Add(i / j);
-                    }
-            }
-            return buddy1.ToString();
+            double pi = 3.14159265358979323846;
+            int[] ans = new int[4];
+            int zeta = 90 + alpha - beta;
+            double zetaR = zeta/180.0*pi;
+            double betaR = beta/180.0 * pi;
+            double gammaR = gamma/180.0 * pi;
+            double K = Math.Sqrt(a * a + b * b - 2 * a * b * Math.Cos(zetaR));
+            double Theta = Math.Acos((a * a - K * K - b * b) / (-2 * K * b));
+            double Kappa = pi/2 + betaR - Theta - gammaR;
+            double d = Math.Sqrt(K*K + c*c -2*K*c*Math.Cos(Kappa));
+            ans[0] = (int)Math.Round(d);
+            double A2 = (Math.Acos((b * b - a * a - K * K) / (-2 * a * K)))/pi*180.0;
+            double A3 = (Math.Acos((c * c - d * d - K * K) / (-2 * d * K)))/pi*180.0;
+            double angle = alpha + A2 + A3;
+            ans[1] = (int)angle;
+            angle = (angle - ans[1])*60;
+            ans[2] = (int)angle;
+            ans[3] = (int)((angle - ans[2]) * 60);
+            return ans;
         }
     }
 }
